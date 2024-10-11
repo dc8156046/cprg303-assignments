d39439eff7e1fa200fb4e891bf3ec13c2385679d
Dan Chen <dan.chen01@edu.sait.ca>
2024-10-11 15:02:10 -0600

    lab 2

--- app/index.js
+++ app/index.js
@@ -1,11 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
+import ToDoList from "../component/ToDoList";
+import ToDoForm from "../component/ToDoForm";
 
 export default function Page() {
   return (
     <View style={styles.container}>
       <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
+        <Text style={styles.subtitle}>To Do List</Text>
+        <ToDoList />
+        <ToDoForm />
       </View>
     </View>
   );
@@ -15,7 +18,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    padding: 24,
+    justifyContent: "center",
   },
   main: {
     flex: 1,
